f4990267e2fd83ac3ab9ac0cb30f0b703c6f3935
samuel907 <samuel.sidivar@gmail.com>
2023-12-01 19:14:39 +0100

    skusanie addeventlisteneru

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,13 @@
 "use strict";
-import ejs from "ejs";
-import express from "express";
+//import ejs from "ejs";
+//import express from "express";
 
 console.log("Hello world");
 console.log("skuska");
 //hello
+
+const searchEl = document.getElementById("searchInput");
+
+searchEl.addEventListener("change", function () {
+  console.log("change occured");
+});
